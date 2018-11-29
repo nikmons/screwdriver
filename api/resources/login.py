ef79c8feca82b1508537b09493f7f5f9a00c60ee
GiwrgosP <gparaskevopoulos@amcstudent.edu.gr>
2018-11-29 18:32:02 +0200

    Update login.py

--- api/resources/login.py
+++ api/resources/login.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, abort, make_response
+from flask import Flask, jsonify, abort, make_response, session
 from flask_restful import Api, Resource, reqparse, fields, marshal
 
 from app import auth, db
