a3ccb1ae57dc85468500e9b9f66047dac223e45a
Nikos Monahogios <nikos13@hotmail.com>
2019-01-10 21:11:09 +0200

    Allow for custom e-mail send

--- api/utils/email_notifier.py
+++ api/utils/email_notifier.py
@@ -1,14 +1,16 @@
 import os
 import requests
 
-def send_simple_message(track_num):
+def send_simple_message(to, subject, content):
     api_key = os.getenv("MAILGUN_API_KEY")
     domain = os.getenv("MAILGUN_DOMAIN")
 
-    return requests.post(
+    resp = requests.post(
         "https://api.mailgun.net/v3/{}/messages".format(domain),
         auth=("api", "{}".format(api_key)),
         data={"from": "MobiRepair <mailgun@{}>".format(domain),
-              "to": ["nikos13@hotmail.com"],
-              "subject": "Hello",
-              "text": "Your tracking number is {}".format(track_num)})
+              "to": [to],
+              "subject": subject,
+              "text": content})
+
+    print(resp.status_code)
